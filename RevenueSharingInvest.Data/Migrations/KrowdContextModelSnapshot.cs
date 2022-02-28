99e3734aad09dba4d87222272396b83ceb0ae4a0
PhatPT <phamtanphat0901@gmail.com>
2022-02-28 16:24:41 +0700

    Add authenticate to swagger

--- RevenueSharingInvest.Data/Migrations/KrowdContextModelSnapshot.cs
+++ RevenueSharingInvest.Data/Migrations/KrowdContextModelSnapshot.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RevenueSharingInvest.Data.Helpers;
 using RevenueSharingInvest.Data.Models.Entities;
 
 namespace RevenueSharingInvest.Data.Migrations
