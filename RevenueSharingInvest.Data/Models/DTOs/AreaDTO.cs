0c1165b005bbc8b17f52669c48799ee7ea680ad5
NguyenKhanh <nguyenkhanh19001069@gmail.com>
2022-05-24 13:34:45 +0700

    [KhanhNHQ] POST method fixed

--- RevenueSharingInvest.Data/Models/DTOs/AreaDTO.cs
+++ RevenueSharingInvest.Data/Models/DTOs/AreaDTO.cs
@@ -8,6 +8,7 @@ namespace RevenueSharingInvest.Data.Models.DTOs
 {
     public class AreaDTO
     {
+        public string id { get; set; }
         public string city { get; set; }
         public string district { get; set; }
         public string ward { get; set; }
