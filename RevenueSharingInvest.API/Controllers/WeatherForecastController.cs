1e78f6b3ffa14fa1face231621c7ef4d4e3cdadc
PhatPT <phamtanphat0901@outlook.com>
2022-10-06 19:24:41 +0700

    done create Pdf file

--- RevenueSharingInvest.API/Controllers/WeatherForecastController.cs
+++ RevenueSharingInvest.API/Controllers/WeatherForecastController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,7 +26,7 @@ namespace RevenueSharingInvest.API.Controllers
         {
             _logger = logger;
         }
-
+/*
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -34,6 +38,32 @@ namespace RevenueSharingInvest.API.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }*/
+
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            var path = Environment.GetEnvironmentVariable("BusinessContract_Path");
+            if (path == null)
+            {
+                Environment.SetEnvironmentVariable("BusinessContract_Path", "C:\\Contracts\\Business-Project");
+                path = Environment.GetEnvironmentVariable("BusinessContract_Path");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            //Initialize PDF writer
+            PdfWriter writer = new PdfWriter(path + "hello_world.pdf");
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(writer);
+            // Initialize document
+            Document document = new Document(pdf);
+            //Add paragraph to the document
+            document.Add(new Paragraph("Hello World!"));
+            //Close document
+            document.Close();
+            return Ok(path);
         }
     }
 }
--- RevenueSharingInvest.Business/Services/Extensions/iText/iTextService.cs
+++ RevenueSharingInvest.Business/Services/Extensions/iText/iTextService.cs
@@ -1,6 +1,13 @@
-﻿using iText.Kernel.Pdf;
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RevenueSharingInvest.Data.Models.Entities;
+using RevenueSharingInvest.Data.Repositories.IRepos;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +16,47 @@ namespace RevenueSharingInvest.Business.Services.Extensions.iText
 {
     public class iTextService
     {
-        public void GenerateBusinessContract()
+
+        private readonly IInvestorRepository _investorService;
+        private readonly IProjectRepository _projectService;
+        public iTextService(IInvestorRepository investorRepository, IProjectRepository projectRepository)
         {
-            PdfWriter writer = new PdfWriter("C:");
+            _investorService = investorRepository;
+            _projectService = projectRepository;
+        }
+
+        private string GetPath()
+        {
+            var path = Environment.GetEnvironmentVariable("BusinessContract_Path");
+            if(path == null) 
+            {
+                Environment.SetEnvironmentVariable("BusinessContract_Path", "C:\\Contracts\\Project-Investor");
+                path = Environment.GetEnvironmentVariable("BusinessContract_Path");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+
+            return path;
+        }
+
+        public async void GenerateProjectContract(string userId, string projectId)
+        {
+            Investor investor = await _investorService.GetInvestorByUserId(Guid.Parse(userId));
+            string path = GetPath();
+
+            //Initialize PDF writer
+            PdfWriter writer = new PdfWriter(path);
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(writer);
+            // Initialize document
+            Document document = new Document(pdf);
+            //Add paragraph to the document
+            document.Add(new Paragraph("Hello World!"));
+            //Close document
+            document.Close();
+            writer.Close();
         }
     }
 }
