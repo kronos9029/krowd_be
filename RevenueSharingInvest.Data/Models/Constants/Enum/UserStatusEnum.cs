1be215423c0b60c6a11be562fa3e96b9df2c2597
NguyenKhanh <nguyenkhanh19001069@gmail.com>
2022-11-15 12:25:34 +0700

    [KhanhNHQ] Fix User GET API

---
+++ RevenueSharingInvest.Data/Models/Constants/Enum/UserStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevenueSharingInvest.Data.Models.Constants.Enum
+{
+    public enum UserStatusEnum
+    {
+        ACTIVE,
+        INACTIVE,
+        BLOCKED
+    }
+}
